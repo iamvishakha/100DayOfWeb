e301ca14a68d173fd578c05b0aadc67aae4602c5
vishakha singh <2000.singhvishakha@gmail.com>
2020-04-27 16:38:06 +0530

    My First Commit

---
+++ Day001/session/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Session</title>
+</head>
+<body>
+   <h1>About Session </h1>
+   <form method="POST" action="session.php">
+   	  <p>
+   	  	<label>Name</label>
+   	  	<input type="text" name="sname">
+   	  </p>
+
+      <p>
+   	  	<label>Password</label>
+   	  	<input type="Password" name="psw">
+   	  </p>
+   	  
+   	  <input type="submit">
+   	  <input type="reset">
+
+   </form>
+</body>
+</html>
